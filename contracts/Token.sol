ea50c7639d234133f46271d6532e2cd6c208acbc
Bui-Alex46 <alex46bui@gmail.com>
2024-03-31 10:02:03 -0700

    Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token{
-    string public name = "Altonio";
-    string public symbol = "ALT"; 
     
+    string public name;
+    string public symbol = "ALT"; 
+    uint256 public decimals = 18;   // Decimals
+    //How many coins in existence
+    // Converting it to Wei
+    uint256 public totalSupply;   // 1,000,000 x 10^18;
+    
+    constructor(string memory _name, 
+                string memory _symbol, 
+                uint256 _totalSupply){
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+
+    }
 }
 
--- test/Token.js
+++ test/Token.js
@@ -1,6 +1,10 @@
 const { ethers } = require("hardhat")
 const { expect } = require("chai");
 
+const tokens = (n) =>{
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 
 describe('Token', () => {
     // Defining scope of token for global use
@@ -9,16 +13,29 @@ describe('Token', () => {
         // Fetch token from blockchain
         const Token = await ethers.getContractFactory('Token')
         // Deploy contract to the test network
-        token = await Token.deploy()
+        token = await Token.deploy('Altonio', 'ALT', '1000000')
     })
-     // Check name is correct
-    it('has correct name', async () => {
-       
-        expect(await token.name()).to.equal('Altonio')
+    
+    // Deployment Block for this token 
+    describe('Deployment', () => {
+        const name = 'Altonio';
+        const symbol = 'ALT';
+        const decimals = '18';
+        const totalSupply = tokens('1000000');
+        // Check name is correct
+        it('has correct name', async () => {
+            expect(await token.name()).to.equal(name)
+        })
+        // Check name is correct
+        it('has correct symbol', async () => {
+            expect(await token.symbol()).to.equal(symbol)
+        })
+        it('has correct decimals', async () => {
+            expect(await token.decimals()).to.equal(decimals)
+        })
+        it('has correct total supply', async () => {
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
     })
-     // Check name is correct
-    it('has correct symbol', async () => {
-       
-        expect(await token.symbol()).to.equal('ALT')
-    })
+    
 })
