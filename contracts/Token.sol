d85933e9a8dd480cc7bdce4fab90b75a17dbbd69
Bui-Alex46 <alex46bui@gmail.com>
2024-04-06 12:41:33 -0700

    Deposit Tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -70,10 +70,9 @@ contract Token{
 
     function transferFrom(address _from, address _to, uint256 _value) 
     public returns (bool success){
-
         // Check Approval
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        require(_value <= balanceOf[_from], 'insufficient balance');
+        require(_value <= allowance[_from][msg.sender], 'insufficient allowance');
         
         // Reset Allowance
         allowance[_from][msg.sender] -= _value;
